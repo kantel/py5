86d4904dcf5c80133e8813d873436ac704448921
Jörg Kantel <joerg@kantel.de>
2022-07-14 14:04:05 +0200

    Create rotatingsquares.py

---
+++ py5_test/rotatingsquares.py
@@ -0,0 +1,27 @@
+WIDTH = 600
+HEIGHT = 600
+
+t = 0
+
+def setup():
+    size(WIDTH, HEIGHT)
+    window_move(1400, 30)
+    window_title("Rotating Squares")
+    rect_mode(CENTER)
+
+def draw():
+    global t
+    background(230, 226, 204)
+    translate(width/2, height/2)    
+    rotate(radians(t))
+    for i in range(12):
+        with push_matrix():
+            translate(200, 0)
+            rotate(radians(2*t))
+            if i%2:
+                fill(160, 51, 46)
+            else:
+                fill(50, 80, 105)
+            rect(0, 0, 50, 50)
+        rotate(radians(360/12))
+    t += 1
