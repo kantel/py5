f7a94d66f074ded96d42188532fa1bd8383217a0
Jörg Kantel <joerg@kantel.de>
2022-08-31 12:53:43 +0200

    Update rotating_triangle_final.py

--- rotating_triangles/rotating_triangle_final.py
+++ rotating_triangles/rotating_triangle_final.py
@@ -1,26 +1,28 @@
-# rotating triangles final
-# after Roger Antonsen (University of Oslo)
-# and Peter Farrell (Math Adventures with Python, p. 93ff.)
-
-def setup():
-    size(600, 600)
-    color_mode(HSB, 100)
+# Rotating Triangles Final
+# nach Roger Antonsen (University of Oslo)
+# und Peter Farrell (Math Adventures with Python, p. 93ff.)
 
 t = 0
 
+def setup():
+    size(600, 600)
+    window_title("Rotierende Dreiecke")
+
 def draw():
     global t
-    background(255, 0, 100)  # white
+    background(30, 30, 30)  # Dark Gray
+    stroke_weight(3)
     translate(width/2, height/2)
+    color_mode(HSB, 100)
     for i in range(90):
         rotate(radians(360/90))
-        push_matrix()
-        translate(200, 0)
-        stroke(i%360, 100, 80)
-        rotate(radians(t + 2*i*360/90))
-        tri(100)
-        pop_matrix()
+        with push_matrix():
+            translate(200, 0)
+            stroke(i%360, 100, 80)
+            rotate(radians(t + 2*i*360/90))
+            tri(100)
     t += 0.5
+    color_mode(RGB, 255)
 
 def tri(length):
     """Zeichnet ein gleichseitiges Dreieck
