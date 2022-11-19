9966b68a09097c148cd7f71ed62620e1d24a79ae
Jörg Kantel <joerg@kantel.de>
2022-11-19 21:09:54 +0100

    Update aquarium.py

--- aquarium/aquarium.py
+++ aquarium/aquarium.py
@@ -1,78 +1,83 @@
 # Mein kleines, bonbonbuntes Aquarium
 # Assets: Fish Pack von Kenney.nl <https://www.kenney.nl/assets/fish-pack>
-from random import choice, randint
+from random import randint
 
 N_FISHES = 25      # Anzahl der Fische
 
-class Actor():
-    
-    def __init__(self, sprite):
-        self.img = load_image("data/" + sprite)
-        self.x = 0
-        self.y = 0
         
-    def update(self):
-        pass
-    
-    def display(self):
-        image(self.img, self.x, self.y)
-        
-class Background(Actor):
+class Background():
     
     def __init__(self, img):
-        Actor.__init__(self, img)
-        
-class Fish(Actor):
+        self.img = load_image("data/" + img + ".png")
     
-    def __init__(self, img, x, y, speed):
-        Actor.__init__(self, img)
-        self.imgstr = img
-        self.img_left0 = self.imgstr[:5] + "l_0.png"
-        self.imgl0 = load_image(self.img_left0)
-        self.img_left1 = self.imgstr[:5] + "l_1.png"
-        self.imgl1 = load_image(self.img_left1)
-        self.img_right0 = self.imgstr[:5] + "r_0.png"
-        self.imgr0 = load_image(self.img_right0)
-        self.img_right1 = self.imgstr[:5] + "r_1.png"
-        self.imgr0 = load_image(self.img_right1)
+    def display(self):
+        image(self.img, 0, 0)
+        
+class Fish():
+    
+    def __init__(self, idx, x, y, dr, speed):
+        self.imgr0 = load_image("data/fish" + str(idx) + "r_0.png")
+        self.imgl0 = load_image("data/fish" + str(idx) + "l_0.png")
+        self.imgr1 = load_image("data/fish" + str(idx) + "r_1.png")
+        self.imgl1 = load_image("data/fish" + str(idx) + "l_1.png")
         self.x = x
         self.y = y
+        self.dir = dr
+        if self.dir == "rt":
+            self.img = self.imgr0
+        elif self.dir == "lt":
+            self.img = self.imgl0
+
         self.speed = speed*randint(1, 3)
+        self.switch = 5
+        self.timer = self.switch
     
     def update(self):
         self.x += self.speed
-        if self.x > 600:
+        if self.timer <= 0:
+            self.timer = self.switch
+            if self.img == self.imgr0:
+                self.img = self.imgr1
+            elif self.img == self.imgr1:
+                self.img = self.imgr0
+            elif self.img == self.imgl0:
+                self.img = self.imgl1
+            elif self.img == self.imgl1:
+                self.img = self.imgl0
+        if self.x > width + randint(40, 200):
             self.img = self.imgl0
-            self.speed = randint(1, 3)
-            self.speed *= -1
-        if self.x < 40:
+            self.y = randint(20, 300)
+            self.speed = randint(-3, -1)
+        if self.x < randint(-200, -40):
             self.img = self.imgr0
-            self.speed *= randint(-3, -1)
+            self.y = randint(20, 300)
+            self.speed = randint(1, 3)
+        self.timer -= 1
         
-    
-fish_images = ["fish1", "fish2", "fish3", "fish4", "fish5", "fish6", "fish7"]
-fishes = []
+    def display(self):
+        image(self.img, self.x, self.y)
 
+fishes = []
+    
 def setup():
     global bg
     size(640, 416)
-    window_title("Jörgs kleines, bonbonbuntes Aquarium")
+    window_title("🐠 Jörgs kleines, bonbonbuntes Aquarium 🐡")
     window_move(1300, 40)
-    bg = Background("background.png")
+    bg = Background("background")
     for _ in range(N_FISHES):
         direction = randint(0, 1)
         if direction == 0:
-            dr = "r"
+            dr = "rt"
             speed = 1
         else:
-            dr = "l"
+            dr = "lt"
             speed = -1
-        x = randint(40, 360)
+        x = randint(-100, width + 200)
         y = randint(20, 300)
-        fish = Fish(choice(fish_images) + dr + "_0.png", x, y, speed)
+        fish = Fish(randint(1, 7), x, y, dr, speed)
         fishes.append(fish)
-    print(fishes[0].img_left0)
-    
+
 def draw():
     background(49, 197, 244)
     bg.display()
