bb8b3f4edcdd98feac46a8453584ee2131fd24a0
Jörg Kantel <joerg@kantel.de>
2024-06-28 00:46:11 +0200

    Conda Env Test

--- py5_test/py5_module_mode_test.py
+++ py5_test/py5_module_mode_test.py
@@ -1,10 +1,11 @@
 import py5
 
 def setup():
-    py5.size(200, 200)
+    py5.size(400, 400)
     py5.rect_mode(py5.CENTER)
     
 def draw():
-    py5.square(py5.mouse_x, py5.mouse_y, 10)
+    py5.fill(py5.color(py5.random_int(5, 250), py5.random_int(5, 250), py5.random_int(5, 250)))
+    py5.square(py5.mouse_x, py5.mouse_y, 20)
 
 py5.run_sketch()
