bf3c7a254ba056681e364bf9bea670a285b1e462
Jörg Kantel <joerg@kantel.de>
2025-07-21 21:27:59 +0200

    Mira-Attraktoren created

---
+++ attractors/miraattraktor.py
@@ -0,0 +1,39 @@
+import py5
+from random import randint
+
+WIDTH, HEIGHT = 640, 480
+ITER = 120000
+RAD = 5
+
+# Parameter
+a = 0.4
+b = 1.0
+
+def setup():
+    py5.size(WIDTH, HEIGHT)
+    py5.window_title("Mira-Abbildung (1)")
+    py5.background(132, 144, 163)
+    py5.stroke_weight(1)
+    py5.no_loop()
+
+def f(x):
+    return a * x - (1.0 - a) * ((2 * (x**2)) / (1.0 + x**2))
+
+def draw():
+    x = 4.0
+    y = 0.0
+    for _ in range(ITER):
+        x1 = b * y + f(x)
+        y = -x + f(x1)
+        x = x1
+        # Skalierungsparameter für die Fenstergröße:
+        xx = int(350 + x*22)
+        yy = int(240 - y*22)
+        red_val = randint(100, 250)
+        green_val = randint(100, 250)
+        blue_val = randint(100, 250)
+        py5.fill(red_val, green_val, blue_val)
+        py5.circle(xx, yy, RAD)
+    print("I did it, Babe!")
+
+py5.run_sketch()
